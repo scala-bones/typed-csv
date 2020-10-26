b3e616f6fe1eb58ad688b2eb1a7d7f6ab649847d
Travis Stevens <mrxtravis@yahoo.com>
2020-10-26 16:07:35 -0400

    working version for part 1 of blog series

--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -1,9 +1,9 @@
 
-// import org.junit.Test
-// import org.junit.Assert._
+import org.junit.Test
+import org.junit.Assert._
 
 class Test1 {
-  // @Test def t1(): Unit = {
-  //   assertEquals("I was compiled by dotty :)", Main.msg)
-  // }
+  @Test def t1(): Unit = {
+    assertEquals("I was compiled by dotty :)", Main.msg)
+  }
 }
